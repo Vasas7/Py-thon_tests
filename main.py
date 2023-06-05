3a143132f1fb618da255352ea5586cbe08a86e67
IndianTechSupp <90865605+Vasas7@users.noreply.github.com>
2023-06-05 09:57:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((600, 600))
 display = pygame.Surface((300, 300))
 
-while True
+while True:
+  display.fill((0, 0, 0))
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+  pygame.transfor.scale(diplay,screen.get_size(), screen)
+  pygame.display.update()
 
